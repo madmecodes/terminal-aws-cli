e39514d3d98b2455b06e8f1bf1b1bfa69d395cca
ayush-gupta-dev <ayushguptadev1@gmail.com>
2024-09-27 02:24:13 +0530

    minor

--- aws-list-profiles.py
+++ aws-list-profiles.py
@@ -41,9 +41,7 @@ def get_aws_profiles():
     # Add or update profiles from credentials file
     for profile in credentials.sections():
         existing = next((p for p in profiles if p["Profile"] == profile), None)
-        if existing:
-            existing["Access Key"] = credentials[profile].get("aws_access_key_id", "Not set")
-        else:
+        if not existing:
             region = "Not set"
             if profile in config:
                 region = config[profile].get("region", "Not set")
@@ -51,11 +49,60 @@ def get_aws_profiles():
             profiles.append({
                 "Profile": profile,
                 "Region": region,
-                "Account ID": account_id,
-                "Access Key": credentials[profile].get("aws_access_key_id", "Not set")
+                "Account ID": account_id
             })
     
     return profiles
+
+# def get_aws_profiles():
+#     """Retrieve all AWS profiles from the config and credentials files."""
+#     config = configparser.ConfigParser()
+#     credentials = configparser.ConfigParser()
+    
+#     # Read AWS config and credentials files
+#     config.read(os.path.expanduser("~/.aws/config"))
+#     credentials.read(os.path.expanduser("~/.aws/credentials"))
+    
+#     profiles = []
+    
+#     # Process profiles from config file
+#     for section in config.sections():
+#         if section.startswith("profile "):
+#             profile_name = section[8:]  # Remove "profile " prefix
+#             region = config[section].get("region", "Not set")
+#             account_id = get_account_id(profile_name)
+#             profiles.append({
+#                 "Profile": profile_name,
+#                 "Region": region,
+#                 "Account ID": account_id
+#             })
+    
+#     # Add or update profiles from credentials file
+#     for profile in credentials.sections():
+#         existing = next((p for p in profiles if p["Profile"] == profile), None)
+#         if existing:
+#             existing["Access Key"] = credentials[profile].get("aws_access_key_id", "Not set")
+#         else:
+#             region = "Not set"
+#             if profile in config:
+#                 region = config[profile].get("region", "Not set")
+#             account_id = get_account_id(profile)
+#             access_key = credentials[profile].get("aws_access_key_id", "Not set")
+            
+#             # Mask all but the last 4 characters of the access key
+#             if access_key != "Not set" and len(access_key) >= 4:
+#                 masked_access_key = "*" * (len(access_key) - 4) + access_key[-4:]
+#             else:
+#                 masked_access_key = 'access_key'  # Handle case where access key is not set or too short
+
+#             profiles.append({
+#                 "Profile": profile,
+#                 "Region": region,
+#                 "Account ID": account_id,
+#                 # "Access Key": masked_access_key[-4:]
+#             })
+    
+#     return profiles
 
 def main():
     profiles = get_aws_profiles()
